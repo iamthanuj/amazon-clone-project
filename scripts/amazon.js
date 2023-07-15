575834e1a9efd16ce2c19e362de4190df4805978
ThanujaFernando <thanujadfernando@gmail.com>
2023-07-15 23:15:10 +0530

    Make changes to input quantity using selector

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product) => {
     </div>
 
     <div class="product-quantity-container ">
-      <select>
+      <select class="js-quantity-selector-${product.id}">
         <option selected value="1">1</option>
         <option value="2">2</option>
         <option value="3">3</option>
@@ -57,11 +57,11 @@ products.forEach((product) => {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
+
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
     button.addEventListener('click', () => {
       const productId = button.dataset.productId;
-
       let matchingItem;
 
       cart.forEach((item) => {
@@ -71,13 +71,15 @@ document.querySelectorAll('.js-add-to-cart')
 
       })
 
+      const selectedQuantity = document.querySelector(`.js-quantity-selector-${productId}`).value
+
       if (matchingItem) {
-        matchingItem.quantity += 1;
+        matchingItem.quantity += Number(selectedQuantity);
       }
       else {
         cart.push({
           productId: productId,
-          quantity: 1
+          quantity: Number(selectedQuantity)
         })
       }
 
